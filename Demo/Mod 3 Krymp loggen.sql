dc6ffa027b2e24c4c8c43ee47446798c8f486774
roblevay <108578793+roblevay@users.noreply.github.com>
2025-09-08 19:05:39 +0200

    Update Mod 3 Krymp loggen.sql

--- Demo/Mod 3 Krymp loggen.sql
+++ Demo/Mod 3 Krymp loggen.sql
@@ -1,3 +1,11 @@
+USE master
+GO
+
+DROP DATABASE IF EXISTS Newdb
+GO
+CREATE DATABASE Newdb
+GO
+
 USE newdb
 CREATE TABLE bigtable
 (
@@ -10,22 +18,26 @@ GO 10000
 
 UPDATE bigtable SET bigcol='y' WHERE bigcol='x'
 UPDATE bigtable SET bigcol='x' WHERE bigcol='y'
-GO 20
+GO 10
+  
+USE  master
+GO
 
 BACKUP DATABASE newdb TO DISK='c:\backup\newdb.bak'--Backup 
---Tac backup många gånger av logfilen
+--Ta backup mÃ¥nga gÃ¥nger av logfilen
 BACKUP LOG newdb TO DISK='c:\backup\newdb1.trn'--Backup 
 BACKUP LOG newdb TO DISK='c:\backup\newdb2.trn'--Backup 
 BACKUP LOG newdb TO DISK='c:\backup\newdb3.trn'--Backup 
 BACKUP LOG newdb TO DISK='c:\backup\newdb4.trn'--Backup 
---Så många som behövs, 
+--SÃ¥ mÃ¥nga som behÃ¶vs, 
 
 --Krymp logfilen
-USE [newdb]
+USE master
 GO
-DBCC SHRINKFILE (N'newdb_log' , 20)--Realistiskt värde
+DBCC SHRINKFILE (N'newdb_log' , 20)--Realistiskt vÃ¤rde
 GO
---Kolla varför inte töms
+--Kolla varfÃ¶r inte tÃ¶ms
 SELECT name, recovery_model_desc, log_reuse_wait_desc
 FROM sys.databases
 WHERE name = 'newdb';
+
