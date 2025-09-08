d2ad7683e2ca805aa62479b3d90dc5496e91b0d9
roblevay <108578793+roblevay@users.noreply.github.com>
2025-09-08 19:37:04 +0200

    Update Mod 3 Krymp loggen.sql

--- Demo/Mod 3 Krymp loggen.sql
+++ Demo/Mod 3 Krymp loggen.sql
@@ -5,6 +5,7 @@ DROP DATABASE IF EXISTS Newdb
 GO
 CREATE DATABASE Newdb
 GO
+BACKUP DATABASE newdb TO DISK='c:\backup\newdb.bak'--Backup av databasen, ska alltid göras
 
 USE newdb
 CREATE TABLE bigtable
@@ -13,31 +14,61 @@ bigcol	CHAR(8000)
 )
 
 
-INSERT INTO bigtable VALUES ('x')
+INSERT INTO bigtable VALUES ('x')--ca 30 s
 GO 10000
 
-UPDATE bigtable SET bigcol='y' WHERE bigcol='x'
+UPDATE bigtable SET bigcol='y' WHERE bigcol='x'--ca 20 s
 UPDATE bigtable SET bigcol='x' WHERE bigcol='y'
-GO 10
+GO 20
   
+--Kolla logstorlek och utnyttjande
+USE newdb;
+SELECT
+  total_log_size_mb       = CAST(total_log_size_in_bytes/1048576.0 AS decimal(19,2)),
+  used_log_space_mb       = CAST(used_log_space_in_bytes/1048576.0  AS decimal(19,2)),
+  free_log_space_mb       = CAST((total_log_size_in_bytes - used_log_space_in_bytes)/1048576.0 AS decimal(19,2)),
+  used_log_space_percent  = used_log_space_in_percent
+FROM sys.dm_db_log_space_usage;
+
+
+
+--1. Ta backup ett par gånger av logfilen (om det behövs)
 USE  master
 GO
+BACKUP LOG newdb TO DISK='c:\backup\newdb1.trn' WITH INIT--Backup 
+BACKUP LOG newdb TO DISK='c:\backup\newdb2.trn' WITH INIT--Backup 
 
-BACKUP DATABASE newdb TO DISK='c:\backup\newdb.bak'--Backup 
---Ta backup många gånger av logfilen
-BACKUP LOG newdb TO DISK='c:\backup\newdb1.trn'--Backup 
-BACKUP LOG newdb TO DISK='c:\backup\newdb2.trn'--Backup 
-BACKUP LOG newdb TO DISK='c:\backup\newdb3.trn'--Backup 
-BACKUP LOG newdb TO DISK='c:\backup\newdb4.trn'--Backup 
---Så många som behövs, 
 
---Krymp logfilen
-USE master
+--Kolla logstorlek och utnyttjande
+USE newdb;
+SELECT
+  total_log_size_mb       = CAST(total_log_size_in_bytes/1048576.0 AS decimal(19,2)),
+  used_log_space_mb       = CAST(used_log_space_in_bytes/1048576.0  AS decimal(19,2)),
+  free_log_space_mb       = CAST((total_log_size_in_bytes - used_log_space_in_bytes)/1048576.0 AS decimal(19,2)),
+  used_log_space_percent  = used_log_space_in_percent
+FROM sys.dm_db_log_space_usage;
+
+
+-- 2. Krymp logfilen
+USE Newdb
 GO
-DBCC SHRINKFILE (N'newdb_log' , 20)--Realistiskt värde
+DBCC SHRINKFILE (N'newdb_log' , EMPTYFILE)
 GO
---Kolla varför inte töms
+
+
+
+--Kolla logstorlek och utnyttjande
+USE newdb;
+SELECT
+  total_log_size_mb       = CAST(total_log_size_in_bytes/1048576.0 AS decimal(19,2)),
+  used_log_space_mb       = CAST(used_log_space_in_bytes/1048576.0  AS decimal(19,2)),
+  free_log_space_mb       = CAST((total_log_size_in_bytes - used_log_space_in_bytes)/1048576.0 AS decimal(19,2)),
+  used_log_space_percent  = used_log_space_in_percent
+FROM sys.dm_db_log_space_usage;
+
+--Om du inte är nöjd, gör om steg 1 och 2 ovan och kolla logstorlek och utnyttjande igen!
+
+--Kolla varför inte töms (vid behov)
 SELECT name, recovery_model_desc, log_reuse_wait_desc
 FROM sys.databases
 WHERE name = 'newdb';
-
