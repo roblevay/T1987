164972d5f442845ce8546d9f38b3af19fcce77e5
roblevay <108578793+roblevay@users.noreply.github.com>
2025-09-09 11:03:30 +0200

    Update Mod 3 Flytta Tempdb.sql

--- Demo/Mod 3 Flytta Tempdb.sql
+++ Demo/Mod 3 Flytta Tempdb.sql
@@ -1,17 +1,18 @@
---Skapa först katalogen D:\SQLData i operativsystemet
+--Skapa fÃ¶rst katalogen C:\SQLData i operativsystemet om den inte finns
 
 USE master;
 GO
 
 ALTER DATABASE tempdb 
-MODIFY FILE (NAME = tempdev, FILENAME = 'D:\SQLData\tempdev.mdf');
+MODIFY FILE (NAME = tempdev, FILENAME = 'C:\SQLData\tempdev.mdf');
 
 ALTER DATABASE tempdb 
-MODIFY FILE (NAME = temp2, FILENAME = 'D:\SQLData\temp2.ndf');
+MODIFY FILE (NAME = temp2, FILENAME = 'C:\SQLData\temp2.ndf');
 
 
 ALTER DATABASE tempdb 
-MODIFY FILE (NAME = templog, FILENAME = 'D:\SQLData\templog.ldf');
+MODIFY FILE (NAME = templog, FILENAME = 'C:\SQLData\templog.ldf');
 GO
+
 
 --Starta nu om servern
